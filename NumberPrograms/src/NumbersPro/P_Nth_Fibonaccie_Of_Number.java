f55f073b39c4bc0f4dc819736805accc73a542ec
Vikas MV <Vikas MV@DESKTOP-3K7CIOH>
2023-04-07 12:41:31 +0530

    v

--- NumberPrograms/src/NumbersPro/P_Nth_Fibonaccie_Of_Number.java
+++ NumberPrograms/src/NumbersPro/P_Nth_Fibonaccie_Of_Number.java
@@ -8,17 +8,15 @@ public class P_Nth_Fibonaccie_Of_Number
 		int nth = 10;
 		int a = 0;
 		int b = 1;
-		
-		int n=0;
 		for (int i = 1; i <= 10; i++) {
 		int c = a + b;
-		System.out.print(a + " ");
-		n=a;
+//		if(nth==i)
+		System.out.print(a + " "); 
 		a = b;
 		b = c;
 		}
 		System.out.println();
 		System.out.println("************************");
-		System.out.println(n);
+
 	}
 }
