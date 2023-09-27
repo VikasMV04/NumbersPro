9a8e9ec2449e9765cfcf1c6164ed421f2b4e57b8
VikasMV04 <126438115+VikasMV04@users.noreply.github.com>
2023-09-27 11:46:16 +0530

    Update C_NeonNumber.java

--- NumberPrograms/src/NumbersPro/C_NeonNumber.java
+++ NumberPrograms/src/NumbersPro/C_NeonNumber.java
@@ -3,7 +3,7 @@ package NumbersPro;
 import java.util.Scanner;
 
 public class C_NeonNumber {
-	//A neon number is a number where the sum of digits of the square of the number is equal to the number
+	//A neon number is a number where the sum of digits of the square of the number is equal to the number. (9 0 1.)
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
